ada39a2286f9afd9f3f489207e01e31b5f51467e
AleksandrFedorov <ol.m.fed@gmail.com>
2023-07-19 02:51:21 +0300

    upp

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import { AuthNav } from "components/AuthNav/AuthNav"
 import { Navigation } from "components/Navigation/Navigation"
-import { UserMenu } from "components/UserMenu/userMenu"
+// import { UserMenu } from "components/UserMenu/userMenu"
 
 export const AppBar = () => {
     return (
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,5 @@
 import { AppBar } from "components/AppBar/AppBar";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export const SharedLayout = () => {
   return (
