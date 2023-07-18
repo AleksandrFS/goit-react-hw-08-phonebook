bd0db47c4f771680f4f504841bb457202f0c162e
AleksandrFedorov <ol.m.fed@gmail.com>
2023-07-19 02:48:36 +0300

    up1

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,12 +3,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
-const setAuthHeader = token => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-}
-const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = '';
-};
+// const setAuthHeader = token => {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// }
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
 
 export const register = createAsyncThunk('auth/register', async (regData, thunkAPI) => {
   try {
