25e42d5034863b419d14cce0c17475bad86eb28a
AleksandrFedorov <ol.m.fed@gmail.com>
2023-07-22 00:42:48 +0300

    up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { persistor, store } from 'redux/store';
 import { Provider } from 'react-redux';
-
-
 import { App } from 'components/App';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -14,9 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goit-react-hw-08-phonebook">
-         
           <App />
-          
         </BrowserRouter>
       </PersistGate>
     </Provider>
