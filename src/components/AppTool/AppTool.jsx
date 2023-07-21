c5c6c11867d44b721abc8df167c9ea444dc4acec
AleksandrFedorov <ol.m.fed@gmail.com>
2023-07-22 00:56:30 +0300

    up

--- src/components/AppTool/AppTool.jsx
+++ src/components/AppTool/AppTool.jsx
@@ -1,8 +1,8 @@
-import { AuthNav } from '../AuthNav/AuthNav';
-import { Navigation } from '../Navigation/Navigation';
+import { AuthNav } from 'components/AuthNav/AuthNav'; 
+import { Navigation } from 'components/Navigation/Navigation'; 
 import { useAuth } from 'hooks/useAuth';
-// import { UserMenu } from 'components/UserMenu/UserMenu';
-import { UserMenu } from '../UserMenu/UserMenu';
+import { UserMenu } from 'components/UserMenu/userMenu';
+
 
 import { AppToolStyled, AuthPartStyled } from './AppTool.styled';
 
