f05aaf157b8085a36e0639344a1811454bc4b83f
AleksandrFedorov <ol.m.fed@gmail.com>
2023-07-22 00:23:46 +0300

    up

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,9 @@
 import { BallTriangle } from 'react-loader-spinner';
-import { Loader } from './Loader.styled';
+import { SpinnerLoad} from './Loader.styled';
 
 export const Spinner = () => {
   return (
-    <Loader>
+    <SpinnerLoad>
       <BallTriangle
         height={50}
         width={50}
@@ -13,6 +13,6 @@ export const Spinner = () => {
         wrapperStyle=""
         visible={true}
       />
-    </Loader>
+    </SpinnerLoad>
   );
 };
